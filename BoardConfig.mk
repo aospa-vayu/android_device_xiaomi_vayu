48e2c8bc01b5517c6f24ab8df72bc07b7c29326a
Ghanish Sharma <ghanishftw@gmail.com>
2022-08-25 20:54:35 +0530

    vayu: hidl: Import manifest, fcm and compatiblity matrix from stock
    Change-Id: I7a4ec3e8f07bf64f3604d31a1047ba397ddefba2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,11 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 BOARD_USES_METADATA_PARTITION := true
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
