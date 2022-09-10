44376da10767058aca0451f9a9a22c09e91cca69
Ghanish <ghanishftw@gmail.com>
2022-09-10 18:56:44 +0530

    vayu: fingerprint: copy hw permissions

--- device.mk
+++ device.mk
@@ -155,6 +155,9 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.vayu \
     libkeymaster_messages.vendor
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
